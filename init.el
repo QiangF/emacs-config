d11aca28640dfb0a52a1a306434029fac0214980
Adi Ratiu <adi@adirat.com>
2012-10-27 17:44:33 +0300

    Indent html using 4 spaces only

--- init.el
+++ init.el
@@ -146,3 +146,9 @@
 (require 'xcscope)
 
 (setq tramp-default-method "ssh")
+
+;; indent html using 4 spaces
+(add-hook 'html-mode-hook
+          (lambda()
+            (setq sgml-basic-offset 4)
+            (setq indent-tabs-mode nil)))
