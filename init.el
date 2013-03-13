756c2ac40492d31981c63e5756a6343bbaf4870b
Adi Ratiu <adi@adirat.com>
2013-03-13 22:25:43 +0200

    Add gdb up/down shortcuts

--- init.el
+++ init.el
@@ -155,3 +155,20 @@
 
 ;; enable erase-buffer command
 (put 'erase-buffer 'disabled nil)
+
+;; Make gdb up/down behave as in terminal
+(add-hook 'gud-mode-hook
+          '(lambda ()
+             (local-set-key [home] ; move to beginning of line, after prompt
+                            'comint-bol)
+             (local-set-key [up] ; cycle backward through command history
+                            '(lambda () (interactive)
+                               (if (comint-after-pmark-p)
+                                   (comint-previous-input 1)
+                                 (previous-line 1))))
+             (local-set-key [down] ; cycle forward through command history
+                            '(lambda () (interactive)
+                               (if (comint-after-pmark-p)
+                                   (comint-next-input 1)
+                                 (forward-line 1))))
+             ))
