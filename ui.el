58e42cf2ea1b67d221ff3466b8dc0ce7dd58fbe5
Adi Ratiu <adi@adirat.com>
2013-04-12 19:35:51 +0300

    Remove whitespace mode

--- ui.el
+++ ui.el
@@ -1,20 +1,8 @@
 (setq inhibit-startup-screen 1) ; Do not show Welcome to emacs buffer
 
-(global-whitespace-mode 1)
 (global-font-lock-mode 1) ; Syntax coloring always on
 
 (load-theme 'zenburn 1)
-
-;; Zenburn changes this, revert tab color to be consistent with whitespace mode
-(set-face-foreground 'whitespace-tab "#4f4f4f")
-(set-face-background 'whitespace-tab nil)
-(set-face-background 'whitespace-space nil)
-;;(set-face-foreground 'whitespace-indentation "#4f4f4f")
-(set-face-background 'whitespace-indentation nil)
-;;(set-face-foreground 'whitespace-space-after-tab "#4f4f4f")
-(set-face-background 'whitespace-space-after-tab nil)
-;;(set-face-foreground 'whitespace-trailing "#4f4f4f")
-(set-face-background 'whitespace-trailing nil)
 
 ;; show either the file or buffer name as the frame title
 (setq frame-title-format
