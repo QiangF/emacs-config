59a7091acc5cedc37fa4ac6a8ab58fb59c99dfa1
Adi Ratiu <adi@adirat.com>
2014-11-17 00:57:44 +0200

    Simplify local file load function

--- init.el
+++ init.el
@@ -1,14 +1,7 @@
-(defconst user-init-dir
-  (cond ((boundp 'user-emacs-directory)
-         user-emacs-directory)
-        ((boundp 'user-init-directory)
-         user-init-directory)
-        (t "~/.emacs.d/")))
-
 (defun load-user-file (file)
   (interactive "f")
   "Load a file in current user's configuration directory"
-  (load-file (expand-file-name file user-init-dir)))
+  (load-file (expand-file-name file user-emacs-directory)))
 
 (load-user-file "packages.el")
 (load-user-file "ui.el")
