2b0da2729c9451f854c880eaa14e1831c5173cde
Adi Ratiu <adi@adirat.com>
2015-06-28 21:04:23 +0300

    Add settings encryption

---
+++ easypg.el
@@ -0,0 +1,11 @@
+(require 'epa)
+
+; (setenv “GPG_AGENT_INFO” nil)
+;(epa-file-enable)
+
+(add-to-list 'load-suffixes ".gpg")
+
+(defun load-my-config ()
+  (interactive)
+  (unless (member 'my-config features)
+    (load-user-file "my-config.gpg")))
--- gnus.el
+++ gnus.el
@@ -4,16 +4,7 @@
       gnus-init-file nil
       gnus-read-newsrc-file nil)
 
-(setq user-full-name "Ioan-Adrian Rațiu")
-(setq user-mail-address "adi@adirat.com")
-
 (setq gnus-select-method '(nntp "news.gmane.org"))
-
-(add-to-list 'gnus-secondary-select-methods
-	     '(nnmaildir "adiratcom" (directory "~/Documents/Mail/adiratcom/") (get-new-mail nil)))
-
-(setq mail-sources '((maildir :path "~/Documents/Mail/adiratcom/" :subdirs ("cur" "new")))
-      mail-source-delete-incoming t)
 
 (setq gnus-read-active-file 'some)
 
@@ -24,17 +15,5 @@
 
 ;(setq gnus-thread-hide-subtree t)
 (setq gnus-thread-ignore-subject t)
-
-(setq send-mail-function 'smtpmail-send-it
-      message-send-mail-function 'smtpmail-send-it
-      message-signature "Ioan-Adrian Rațiu"
-      smtpmail-starttls-credentials '(("smtp.gmail.com" 587 nil nil))
-      smtpmail-auth-credentials '(("smtp.gmail.com" 587
-				   "adi@adirat.com" nil))
-      smtpmail-default-smtp-server "smtp.gmail.com"
-      smtpmail-smtp-server "smtp.gmail.com"
-      smtpmail-smtp-service 587
-      smtpmail-local-domain "adirat.com"
-      gnus-ignored-newsgroups "^to\\.\\|^[0-9. ]+\\( \\|$\\)\\|^[\"]\"[#'()]")
 
 (setq mm-discouraged-alternatives '("text/html" "text/richtext"))
