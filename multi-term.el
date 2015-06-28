204817aefc7f11723dbdd29ba7f6e67ce9baabe4
Adi Ratiu <adi@adirat.com>
2015-06-28 14:14:14 +0300

    Add multi-term keybinds

--- multi-term.el
+++ multi-term.el
@@ -2,3 +2,7 @@
 
 (setq multi-term-program "/bin/bash")
 (setq term-unbind-key-list '("C-z" "C-x" "C-c" "C-t" "C-h" "C-y" "C-v" "<ESC>"))
+
+(global-set-key (kbd "<f1>") 'multi-term)
+(global-set-key (kbd "<f2>") 'multi-term-prev)
+(global-set-key (kbd "<f3>") 'multi-term-next)
