d6bfb0b29faf1cb11d4c506eb41d18d153f5f22c
Ioan-Adrian Ratiu <adi@adirat.com>
2015-06-29 15:16:40 +0300

    Dired: Bind dired-jump cmds to F5, F6
    Signed-off-by: Ioan-Adrian Ratiu <adi@adirat.com>

--- dired.el
+++ dired.el
@@ -1,3 +1,8 @@
+(require 'dired-x)
+
 (setq dired-listing-switches "-alh")
 
 (put 'dired-find-alternate-file 'disabled nil) ; allow 'a' cmd
+
+(global-set-key (kbd "<f5>") 'dired-jump)
+(global-set-key (kbd "<f6>") 'dired-jump-other-window)
