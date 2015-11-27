da9d13a5320d3239ae177c0959769e706645a273
Ioan-Adrian Ratiu <adi@adirat.com>
2015-11-27 16:18:44 +0200

    commands: show matching paren in minibuf if offscreen

--- commands.el
+++ commands.el
@@ -70,3 +70,15 @@ If the current buffer is not associated with a file, its a error."
     (load-user-file "config.gpg")))
 
 (setq password-cache-expiry nil)
+
+(defadvice show-paren-function
+    (after show-matching-paren-offscreen activate)
+  "If the matching paren is offscreen, show the matching line in the
+        echo area. Has no effect if the character before point is not of
+        the syntax class ')'."
+  (interactive)
+  (let* ((cb (char-before (point)))
+	 (matching-text (and cb
+			     (char-equal (char-syntax cb) ?\) )
+			     (blink-matching-open))))
+    (when matching-text (message matching-text))))
