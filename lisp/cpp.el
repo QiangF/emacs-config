e6fe9ecaf4e0962f287b9a6ade6685324995d9fe
Ioan-Adrian Ratiu <adi@adirat.com>
2016-06-06 15:37:31 +0300

    cpp.el: remove ggtags
    Signed-off-by: Ioan-Adrian Ratiu <adi@adirat.com>

--- lisp/cpp.el
+++ lisp/cpp.el
@@ -1,14 +1,7 @@
-(require 'ggtags)
-
 (setq-default c-basic-offset 8)
 
 (add-hook 'initialization-hook
           (lambda ()
             (c-set-style "linux")))
-
-(add-hook 'c-mode-common-hook
-          (lambda ()
-            (when (derived-mode-p 'c-mode 'c++-mode 'java-mode 'asm-mode)
-              (ggtags-mode 1))))
 
 (which-function-mode 1)
