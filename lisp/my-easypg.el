7118bcb385d48eeb5dd581079c70f528f8f485b5
Ioan-Adrian Ratiu <adi@adirat.com>
2016-11-07 23:04:58 +0200

    my-easypg: improve gpg-related warnings
    Signed-off-by: Ioan-Adrian Ratiu <adi@adirat.com>

--- lisp/my-easypg.el
+++ lisp/my-easypg.el
@@ -5,8 +5,9 @@
 (defvar have-private-key
   (string= (shell-command-to-string "gpg --list-secret-keys | grep 720FD288FB5BBF5DE3A4BEBA92C6AB6D6C229F93 | wc -l") "1\n"))
 
-(when have-private-key
-  (add-to-list 'load-suffixes ".el.gpg"))
+(if have-private-key
+    (add-to-list 'load-suffixes ".el.gpg")
+  (message "WARNING: Private GPG key not found"))
 
 (defun read-gpg-file (file)
   (let ((decrypt-file (expand-file-name file user-emacs-directory))
@@ -16,5 +17,6 @@
       (epg-decrypt-file ctx decrypt-file nil))))
 
 (defun load-gpg (file)
-  (if have-private-key (load file)
-    (message "Couldn't load %s: No gpg key found" file)))
+  (if have-private-key
+      (load file)
+    (message "WARNING: Couldn't load %s (No gpg key found)" file)))
