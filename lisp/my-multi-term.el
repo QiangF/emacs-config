5d2a2ee680a95018c3163ab12419f42cd1d71c96
Ioan-Adrian Ratiu <adi@adirat.com>
2016-11-17 17:38:21 +0200

    multi-term: do not use global keys
    Signed-off-by: Ioan-Adrian Ratiu <adi@adirat.com>

--- lisp/my-multi-term.el
+++ lisp/my-multi-term.el
@@ -4,7 +4,7 @@
 (setq multi-term-program "/bin/bash")
 (setq term-unbind-key-list '("C-z" "C-x" "C-c" "C-t" "C-h" "C-y" "C-v" "<ESC>"))
 
-(global-set-key (kbd "<f5>") 'multi-term)
-(global-set-key (kbd "C-x t") 'helm-mt)
+;(global-set-key (kbd "<f5>") 'multi-term)
+;(global-set-key (kbd "C-x t") 'helm-mt)
 
 (helm-mt/wrap-shells t)
