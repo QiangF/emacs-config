236c7381cdc85063352290ca220b61b7c9704c08
Ioan-Adrian Ratiu <adi@adirat.com>
2016-11-29 16:06:48 +0200

    my-multi-term: remove unused code
    Signed-off-by: Ioan-Adrian Ratiu <adi@adirat.com>

--- lisp/my-multi-term.el
+++ lisp/my-multi-term.el
@@ -1,14 +1,11 @@
 (require 'multi-term)
-(require 'helm-mt)
+;(require 'helm-mt)
 (require 'term)
 
 (setq multi-term-program "/bin/bash")
 (setq term-unbind-key-list '("C-z" "C-x" "C-c" "C-t" "C-h" "C-y" "C-v" "<ESC>"))
 
-;(global-set-key (kbd "<f5>") 'multi-term)
-;(global-set-key (kbd "C-x t") 'helm-mt)
-
-(helm-mt/wrap-shells t)
+;(helm-mt/wrap-shells t)
 
 (defun term-toggle-mode ()
   "Toggles term between line mode and char mode"
