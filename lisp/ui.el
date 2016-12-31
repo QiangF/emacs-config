b95dfb3f96da179ab904521d858cf3124253edb7
Ioan-Adrian Ratiu <adi@adirat.com>
2016-12-31 03:53:03 +0200

    lisp/ui: always start emacs frames maximized
    Signed-off-by: Ioan-Adrian Ratiu <adi@adirat.com>

--- lisp/ui.el
+++ lisp/ui.el
@@ -11,8 +11,9 @@
 
 (set-frame-font "DejaVu Sans Mono-11" t t)
 
-(add-to-list 'default-frame-alist '(height . 42))
-(add-to-list 'default-frame-alist '(width . 80))
+;(add-to-list 'default-frame-alist '(height . 42))
+;(add-to-list 'default-frame-alist '(width . 80))
+(add-to-list 'default-frame-alist '(fullscreen . maximized))
 
 (setq make-backup-files nil) ;don't create backup~
 (setq auto-save-default nil) ;don't create #autosave#
