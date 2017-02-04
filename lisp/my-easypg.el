e66b11563e4065323ec334bdaab7353b5a0d9904
Ioan-Adrian Ratiu <adi@adirat.com>
2017-02-04 16:51:00 +0200

    my-easypg: move code after definitions
    Signed-off-by: Ioan-Adrian Ratiu <adi@adirat.com>

--- lisp/my-easypg.el
+++ lisp/my-easypg.el
@@ -1,13 +1,7 @@
 (require 'epa)
 
-(setq password-cache-expiry nil)
-
 (defvar have-private-key
   (string= (shell-command-to-string "gpg --list-secret-keys | grep 720FD288FB5BBF5DE3A4BEBA92C6AB6D6C229F93 | wc -l") "1\n"))
-
-(if have-private-key
-    (add-to-list 'load-suffixes ".el.gpg")
-  (message "WARNING: Private GPG key not found"))
 
 (defun read-gpg-file (file)
   (let ((file-to-decrypt (expand-file-name file user-emacs-directory))
@@ -27,3 +21,9 @@
   (if have-private-key
       (load file)
     (message "WARNING: Couldn't load %s (No gpg key found)" file)))
+
+(setq password-cache-expiry nil)
+
+(if have-private-key
+    (add-to-list 'load-suffixes ".el.gpg")
+  (message "ERROR: Private GPG key not found"))
