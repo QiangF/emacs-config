b029068ad88fd107266766a11ae6877643594817
Ioan-Adrian Ratiu <adi@adirat.com>
2017-03-14 23:15:29 +0200

    ui.el: remove show-paren-function advice
    Broken and unused
    
    Signed-off-by: Ioan-Adrian Ratiu <adi@adirat.com>

--- lisp/ui.el
+++ lisp/ui.el
@@ -116,15 +116,3 @@
 ;(global-hl-line-mode 1)
 
 (setq font-lock-maximum-decoration nil)
-
-(defadvice show-paren-function
-    (after show-matching-paren-offscreen activate)
-  "If the matching paren is offscreen, show the matching line in the
-        echo area. Has no effect if the character before point is not of
-        the syntax class ')'."
-  (interactive)
-  (let* ((cb (char-before (point)))
-	 (matching-text (and cb
-			     (char-equal (char-syntax cb) ?\) )
-			     (blink-matching-open))))
-    (when matching-text (message matching-text))))
