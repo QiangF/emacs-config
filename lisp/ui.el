385ee5b64b32e7416d0b778551449da41e97a802
Ioan-Adrian Ratiu <adi@adirat.com>
2017-03-17 23:07:54 +0200

    Revert "ui.el: set window move keys to fn keys"
    This reverts commit b5cb9251b993e602b7564c02637112813d7fcdcc.
    
    Using fn keys was a bad idea after all.
    
    Signed-off-by: Ioan-Adrian Ratiu <adi@adirat.com>

--- lisp/ui.el
+++ lisp/ui.el
@@ -110,22 +110,19 @@
 
 (delete-selection-mode 1)
 
-(global-unset-key (kbd "C-x o"))
-(global-unset-key (kbd "<f1>"))
-
 (setq winum-keymap
       (let ((map (make-sparse-keymap)))
-	(define-key map (kbd "<f1>") 'winum-select-window-1)
-	(define-key map (kbd "<f2>") 'winum-select-window-2)
-	(define-key map (kbd "<f3>") 'winum-select-window-3)
-	(define-key map (kbd "<f4>") 'winum-select-window-4)
-	(define-key map (kbd "<f5>") 'winum-select-window-5)
-	(define-key map (kbd "<f6>") 'winum-select-window-6)
-	(define-key map (kbd "<f7>") 'winum-select-window-7)
-	(define-key map (kbd "<f8>") 'winum-select-window-8)
-	(define-key map (kbd "<f9>") 'winum-select-window-8)
-	(define-key map (kbd "<f10>") 'winum-select-window-0-or-10)
-	(define-key map (kbd "<f11>") 'winum-select-window-by-number)
+	(define-key map (kbd "C-`") 'winum-select-window-by-number)
+	(define-key map (kbd "C-²") 'winum-select-window-by-number)
+	(define-key map (kbd "M-0") 'winum-select-window-0-or-10)
+	(define-key map (kbd "M-1") 'winum-select-window-1)
+	(define-key map (kbd "M-2") 'winum-select-window-2)
+	(define-key map (kbd "M-3") 'winum-select-window-3)
+	(define-key map (kbd "M-4") 'winum-select-window-4)
+	(define-key map (kbd "M-5") 'winum-select-window-5)
+	(define-key map (kbd "M-6") 'winum-select-window-6)
+	(define-key map (kbd "M-7") 'winum-select-window-7)
+	(define-key map (kbd "M-8") 'winum-select-window-8)
 	map))
 
 (require 'winum)
@@ -143,5 +140,7 @@
 (global-set-key (kbd "S-C-<right>") 'enlarge-window-horizontally)
 (global-set-key (kbd "S-C-<up>") 'shrink-window)
 (global-set-key (kbd "S-C-<down>") 'enlarge-window)
+
+(global-unset-key "\C-x\o")
 
 (setq font-lock-maximum-decoration nil)
