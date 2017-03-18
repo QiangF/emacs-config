6929cc39f5696a1300f0008656296c91ef1c5140
Ioan-Adrian Ratiu <adi@adirat.com>
2017-03-19 01:44:44 +0200

    my-easypg: set epa-progress-callback-function to nil (reduce noise)
    Signed-off-by: Ioan-Adrian Ratiu <adi@adirat.com>

--- lisp/my-easypg.el
+++ lisp/my-easypg.el
@@ -20,6 +20,9 @@
       (load file)
     (message "WARNING: Couldn't load %s (No gpg key found)" file)))
 
+
+(defun epa-progress-callback-function (_context what _char current total handback) nil)
+
 ; load this in a post-frame hook because gpg-agent asks for a password on first
 ; startup and caches it. Don't want emacs daemon to hang because of gpg-agent.
 (defun load-private-data ()
