6a4692db4f1d0b5b8551dc4a0c0fae941799882a
Ioan-Adrian Ratiu <adi@adirat.com>
2017-08-03 18:57:48 +0300

    w3m: remove .el file
    Signed-off-by: Ioan-Adrian Ratiu <adi@adirat.com>

--- lisp/w3m.el
+++
@@ -1,8 +0,0 @@
-(require 'w3m)
-
-(global-set-key "\C-xm" 'browse-url-at-point)
-
-(setq browse-url-browser-function 'w3m-browse-url)
-(setq w3m-use-cookies t)
-
-(autoload 'w3m-browse-url "w3m" "Ask a WWW browser to show a URL." t)
