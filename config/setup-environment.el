27f2c3d67970472b30e7e5ef913e78a77b510a55
Ioan-Adrian Ratiu <adi@adirat.com>
2017-08-15 22:24:45 +0300

    init.el: move libdir load-path call to config/environment
    Signed-off-by: Ioan-Adrian Ratiu <adi@adirat.com>

--- config/setup-environment.el
+++ config/setup-environment.el
@@ -1,9 +1,18 @@
 (setq user-emacs-directory (expand-file-name user-emacs-directory)
       temporary-file-directory (concat user-emacs-directory "tmp/")
       config-file-directory (concat user-emacs-directory "config/")
+      lib-file-directory (concat user-emacs-directory "lib")
       binary-file-directory (concat user-emacs-directory "bin"))
 
 (setq exec-path (cons binary-file-directory exec-path))
 
 (unless (string-match-p (regexp-quote binary-file-directory) (getenv "PATH"))
   (setenv "PATH" (concat (getenv "PATH") ":" binary-file-directory)))
+
+(let ((lib-paths (directory-files lib-file-directory t directory-files-no-dot-files-regexp))
+      (non-std-lib-paths (mapcar (apply-partially 'concat user-emacs-directory)
+			      '("lib/magit/lisp"
+				"lib/notmuch/emacs"
+				"lib/geiser/elisp"
+				"lisp"))))
+  (setq load-path (append load-path non-std-lib-paths lib-paths)))
--- init.el
+++ init.el
@@ -1,15 +1,6 @@
-;(package-initialize)
+;(package-initipalize)
 
-(load (concat user-emacs-directory "lisp/my-env"))
-
-(let* ((libdir (concat user-emacs-directory "lib"))
-       (lib-paths (directory-files libdir t directory-files-no-dot-files-regexp))
-       (non-std-lib-paths (mapcar (apply-partially 'concat user-emacs-directory)
-			      '("lib/magit/lisp"
-				"lib/notmuch/emacs"
-				"lib/geiser/elisp"
-				"lisp"))))
-  (setq load-path (append load-path non-std-lib-paths lib-paths)))
+(load (concat user-emacs-directory "config/setup-environment"))
 
 (require 'find-lisp)
 (mapc 'load (find-lisp-find-files (concat user-emacs-directory "lisp") "\\.el$"))
