eb4d2960c322ffc7f3e808bfa37b47e0ca8b941f
Ioan-Adrian Ratiu <adi@adirat.com>
2017-12-21 06:25:28 +0200

    easypg: simplify & run without pinentry.el
    Signed-off-by: Ioan-Adrian Ratiu <adi@adirat.com>

--- lisp/my-easypg.el
+++ lisp/my-easypg.el
@@ -1,11 +1,16 @@
-(require 'pinentry)
 (require 'epa)
+
+(start-process "gpg-agent" nil "gpg-agent" "--daemon")
+
+(setenv "SSH_AUTH_SOCK"
+	(concat "/run/user/" (number-to-string (user-uid)) "/gnupg/S.gpg-agent.ssh"))
+
+(setq password-cache-expiry nil)
+
+(setq epa-pinentry-mode 'loopback)
 
 (defvar have-private-key
   (string= (shell-command-to-string "gpg --list-secret-keys | grep 720FD288FB5BBF5DE3A4BEBA92C6AB6D6C229F93 | wc -l") "1\n"))
-
-(defvar gpg-agent-ssh-sock
-  (concat "/run/user/" (number-to-string (user-uid)) "/gnupg/S.gpg-agent.ssh"))
 
 (defun read-gpg-file (file)
   (let ((file-to-decrypt (expand-file-name file user-emacs-directory))
@@ -18,25 +23,16 @@
 (defun epa-progress-callback-function (_context what _char current total handback) nil)
 
 ; load this in a post-frame hook because gpg-agent asks for a password on first
-; startup and caches it. Don't want emacs daemon to hang because of gpg-agent.
+; startup and caches it. Don't want emacs daemon to hang because of no gpg-agent.
 (defun load-private-data ()
   (interactive)
   (if (not have-private-key)
       (message "ERROR: Private GPG key not found")
-    (start-process "gpg-agent" nil "gpg-agent" "--daemon")
-    (setenv "SSH_AUTH_SOCK" gpg-agent-ssh-sock)
-    (setq password-cache-expiry nil
-	  pinentry--socket-dir temporary-file-directory)
-    (unless (file-exists-p (concat pinentry--socket-dir "pinentry"))
-      (pinentry-start)
-      (add-hook 'kill-emacs-hook 'pinentry-stop))
     (add-to-list 'load-suffixes ".el.gpg")
-    (load "private-config")
-    (when (get-buffer "*Pinentry*")
-      (kill-buffer "*Pinentry*"))))
+    (load "private-config")))
 
 (defun first-frame-hook (frame)
   (remove-hook 'after-make-frame-functions 'first-frame-hook)
-  (run-at-time "80 millisec" nil 'load-private-data))
+  (run-at-time nil nil 'load-private-data))
 
 (add-hook 'after-make-frame-functions 'first-frame-hook)
