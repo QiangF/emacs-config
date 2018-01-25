25282e6ed07267bbfa8c286059ddd8d839aefab6
Ioan-Adrian Ratiu <adi@adirat.com>
2018-01-25 03:26:17 +0200

    my-tramp: rename persistency-file-name & add histfile-override
    Signed-off-by: Ioan-Adrian Ratiu <adi@adirat.com>

--- lisp/my-tramp.el
+++ lisp/my-tramp.el
@@ -1,6 +1,6 @@
 (require 'tramp-cache)
 
-(setq tramp-persistency-file-name
-      (concat temporary-file-directory "tramp"))
+(setq tramp-persistency-file-name (concat temporary-file-directory "tramp-persistency")
+      tramp-histfile-override (concat temporary-file-directory "tramp-history"))
 
 (setq tramp-default-method "ssh")
