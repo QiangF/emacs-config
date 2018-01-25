94190de9adc5b6a3b92d9fb42d1f2beda4780e35
Ioan-Adrian Ratiu <adi@adirat.com>
2018-01-25 04:38:04 +0200

    org-mode: don't spam tmp with babel-* dirs on each session
    Signed-off-by: Ioan-Adrian Ratiu <adi@adirat.com>

--- lisp/org-mode.el
+++ lisp/org-mode.el
@@ -1,2 +1,6 @@
-(require 'org)
+(setq org-babel-temporary-directory (concat temporary-file-directory "babel"))
 
+(unless (file-directory-p org-babel-temporary-directory)
+  (dired-create-directory org-babel-temporary-directory))
+
+(require 'org)
