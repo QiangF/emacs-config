39046bc61fe8148c8eae2cb536c07b190ee4c11c
Ioan-Adrian Ratiu <adi@adirat.com>
2018-01-25 03:14:34 +0200

    haskell.el: delete (not writing haskell anymore :)
    Signed-off-by: Ioan-Adrian Ratiu <adi@adirat.com>

--- lisp/haskell.el
+++
@@ -1,7 +0,0 @@
-(require 'haskell-mode)
-
-(add-hook 'haskell-mode-hook 'turn-on-haskell-indentation)
-
-(add-to-list 'completion-ignored-extensions ".hi")
-(add-to-list 'completion-ignored-extensions ".dyn_hi")
-(add-to-list 'completion-ignored-extensions ".dyn_o")
