37800863e56ccb31ca48c396b17534456f94274a
Ioan-Adrian Ratiu <adi@adirat.com>
2018-01-30 19:30:40 +0200

    dired: unbind C-x C-f and make C-l global
    Signed-off-by: Ioan-Adrian Ratiu <adi@adirat.com>

--- lisp/my-dired.el
+++ lisp/my-dired.el
@@ -4,6 +4,5 @@
 
 (put 'dired-find-alternate-file 'disabled nil) ; allow 'a' cmd
 
-(define-key dired-mode-map (kbd "C-l") 'dired-jump)
-
-(global-set-key (kbd "C-x C-f") 'dired-jump)
+(global-set-key (kbd "C-l") 'dired-jump)
+(global-set-key (kbd "C-x C-f") nil)
