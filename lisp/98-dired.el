0c9fd4a8ad98936538fbeacef0faba7b298ba802
Ioan-Adrian Ratiu <adi@adirat.com>
2018-02-02 18:15:58 +0200

    dired: don't prompt on recursive deletes
    Signed-off-by: Ioan-Adrian Ratiu <adi@adirat.com>

--- lisp/98-dired.el
+++ lisp/98-dired.el
@@ -1,6 +1,7 @@
 (require 'dired-x)
 
-(setq dired-listing-switches "-alh")
+(setq dired-listing-switches "-alh"
+      dired-recursive-deletes 'always)
 
 (put 'dired-find-alternate-file 'disabled nil) ; allow 'a' cmd
 
