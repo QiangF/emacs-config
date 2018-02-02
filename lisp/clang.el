3c2619e08445c51018e044032ad7a61232d5530f
Ioan-Adrian Ratiu <adi@adirat.com>
2018-02-02 17:19:32 +0200

    lisp/clang: remove helm logic
    Signed-off-by: Ioan-Adrian Ratiu <adi@adirat.com>

--- lisp/clang.el
+++ lisp/clang.el
@@ -1,25 +1,5 @@
 (global-unset-key "\C-t")
 
-(add-hook 'c-mode-hook 'helm-gtags-mode)
-
-(setq
- helm-gtags-ignore-case t
- helm-gtags-auto-update t
- helm-gtags-use-input-at-cursor t
- helm-gtags-pulse-at-cursor t
- helm-gtags-prefix-key "\C-cg"
- helm-gtags-suggested-key-mapping t
- )
-
-(eval-after-load "helm-gtags"
-  '(progn
-     (define-key helm-gtags-mode-map (kbd "C-c g a") 'helm-gtags-tags-in-this-function)
-     (define-key helm-gtags-mode-map (kbd "C-j") 'helm-gtags-select)
-     (define-key helm-gtags-mode-map (kbd "M-.") 'helm-gtags-dwim)
-     (define-key helm-gtags-mode-map (kbd "M-,") 'helm-gtags-pop-stack)
-     (define-key helm-gtags-mode-map (kbd "C-c <") 'helm-gtags-previous-history)
-     (define-key helm-gtags-mode-map (kbd "C-c >") 'helm-gtags-next-history)))
-
 (setq-default c-basic-offset 8)
 
 (add-hook 'initialization-hook
