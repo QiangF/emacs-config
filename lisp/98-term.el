dc082b34e0c6736d58a339e60960f56910c3b9c7
Ioan-Adrian Ratiu <adi@adirat.com>
2018-02-05 16:43:39 +0200

    term: remove unnecessary code
    Don't use these as often as I though when added them so I'll just call
    "term-char/line-mode" to switch and keep the conf clean.
    
    Signed-off-by: Ioan-Adrian Ratiu <adi@adirat.com>

--- lisp/98-term.el
+++ lisp/98-term.el
@@ -1,17 +1,4 @@
 (require 'multi-term)
-(require 'term)
 
 (setq multi-term-program "/bin/bash")
 (setq term-unbind-key-list '("C-z" "C-x" "C-c" "C-t" "C-h" "C-y" "C-v" "<ESC>"))
-
-(defun term-toggle-mode ()
-  "Toggles term between line mode and char mode"
-  (interactive)
-  (if (term-in-line-mode)
-      (term-char-mode)
-    (term-line-mode)))
-
-(define-key term-mode-map (kbd "C-c C-j") 'term-toggle-mode)
-(define-key term-mode-map (kbd "C-c C-k") 'term-toggle-mode)
-(define-key term-raw-map (kbd "C-c C-j") 'term-toggle-mode)
-(define-key term-raw-map (kbd "C-c C-k") 'term-toggle-mode)
