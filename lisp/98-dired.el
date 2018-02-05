1b4de7ec7649393a9c7214bfbfff8964e514d061
Ioan-Adrian Ratiu <adi@adirat.com>
2018-02-05 18:02:09 +0200

    dired: bind C-x C-l same as C-l
    Out of habbit I confuse these quite a lot so bind them to the same sequence.
    
    Signed-off-by: Ioan-Adrian Ratiu <adi@adirat.com>

--- lisp/98-dired.el
+++ lisp/98-dired.el
@@ -6,4 +6,5 @@
 (put 'dired-find-alternate-file 'disabled nil) ; allow 'a' cmd
 
 (global-set-key (kbd "C-l") 'dired-jump)
+(global-set-key (kbd "C-x C-l") 'dired-jump)
 (global-set-key (kbd "C-x C-f") nil)
