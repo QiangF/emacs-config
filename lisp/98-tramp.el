58ef50ae6a9a6f12be144f930506fd6259e56cf0
Ioan-Adrian Ratiu <adi@adirat.com>
2018-03-12 00:33:38 +0200

    tramp: set hist-file-override to nil
    Signed-off-by: Ioan-Adrian Ratiu <adi@adirat.com>

--- lisp/98-tramp.el
+++ lisp/98-tramp.el
@@ -1,6 +1,6 @@
 (require 'tramp-cache)
 
 (setq tramp-persistency-file-name (concat temporary-file-directory "tramp-persistency")
-      tramp-histfile-override (concat temporary-file-directory "tramp-history"))
+      tramp-histfile-override nil)
 
 (setq tramp-default-method "ssh")
