0e3b09c24d03723f690f94dcee8f1b9ce701f97a
Ioan-Adrian Ratiu <adi@adirat.com>
2018-06-03 02:40:04 +0300

    notmuch: fix mail download logic to use ido
    Signed-off-by: Ioan-Adrian Ratiu <adi@adirat.com>

--- lisp/20-notmuch.el
+++ lisp/20-notmuch.el
@@ -125,16 +125,10 @@
 
 (defun notmuch-download-message-filter (proc string)
   (let* ((full-remote-name (replace-regexp-in-string "\n\\'" "" string))
-	 (mail-name (file-name-nondirectory full-remote-name))
-	 (local-dir (read-from-minibuffer "Destination directory: "))
-	 (local-name (read-from-minibuffer "File name: "))
-	 (full-dir (if (eq (substring local-dir -1) "/") local-dir (concat local-dir "/")))
-	 (full-local-name (concat full-dir local-name)))
-;    TODO: Fix case when directory doesn't exist because I removed helm entirely
-;    (when (and local-dir (not (file-exists-p local-dir)))
-;      (helm-ff--mkdir local-dir))
-    (start-process "mail-scp" nil "mail-scp" full-remote-name full-local-name)
-    (message "Succesfully saved %s" full-local-name)))
+	   (mail-name (file-name-nondirectory full-remote-name))
+	   (local-name (expand-file-name (ido-read-file-name "enter file: "))))
+    (start-process "mail-scp" nil "mail-scp" full-remote-name local-name)
+    (message "Succesfully saved %s" local-name)))
 
 (defun notmuch-download-message ()
   (interactive)
