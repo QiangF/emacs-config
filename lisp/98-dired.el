d619f1f7ba70eca315d1b77c1c1f4d9e03a0f6cb
Ioan-Adrian Ratiu <adi@adirat.com>
2018-06-15 05:33:41 +0300

    dired: unbind C-l from dired-jump
    Signed-off-by: Ioan-Adrian Ratiu <adi@adirat.com>

--- lisp/98-dired.el
+++ lisp/98-dired.el
@@ -4,6 +4,3 @@
       dired-recursive-deletes 'always)
 
 (put 'dired-find-alternate-file 'disabled nil) ; allow 'a' cmd
-
-(global-set-key (kbd "C-l") 'dired-jump)
-(global-set-key (kbd "C-x C-l") 'dired-jump)
