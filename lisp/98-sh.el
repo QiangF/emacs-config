4afd64fc604a1cc6db460b5ee08f320856b5440b
Ioan-Adrian Ratiu <adi@adirat.com>
2018-07-29 14:55:23 +0300

    sh: add basic shell-mode completion
    Signed-off-by: Ioan-Adrian Ratiu <adi@adirat.com>

--- lisp/98-sh.el
+++ lisp/98-sh.el
@@ -1,3 +1,6 @@
 ;; always mark sh scripts executable on save
 (add-hook 'after-save-hook
           'executable-make-buffer-file-executable-if-script-p)
+
+(require 'bash-completion)
+(bash-completion-setup)
