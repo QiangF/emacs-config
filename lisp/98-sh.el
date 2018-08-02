ea91ac96445f6f9696ca8e19254ca0619e58c7e1
Ioan-Adrian Ratiu <adi@adirat.com>
2018-08-02 16:50:34 +0300

    sh/term: consolidate all configs in one file
    Signed-off-by: Ioan-Adrian Ratiu <adi@adirat.com>

--- lisp/98-sh.el
+++ lisp/98-sh.el
@@ -1,4 +1,35 @@
-;; always mark sh scripts executable on save
+(require 'xterm-color)
+(require 'eshell)
+
+(setenv "TERM" "st-256color")
+
+(setq comint-terminfo-terminal "st-256color"
+      comint-prompt-read-only t)
+
+(setq comint-output-filter-functions
+      (remove 'ansi-color-process-output
+	      comint-output-filter-functions))
+
+(setq eshell-output-filter-functions
+      (remove 'eshell-handle-ansi-color
+	      eshell-output-filter-functions))
+
+(add-to-list 'eshell-preoutput-filter-functions 'xterm-color-filter)
+
+(add-hook 'shell-mode-hook
+	  (lambda ()
+	    (add-hook 'comint-preoutput-filter-functions 'xterm-color-filter nil t)
+	    (font-lock-mode 0)))
+
+(add-hook 'eshell-before-prompt-hook
+	  (lambda ()
+	    (setq xterm-color-preserve-properties t)
+	    (font-lock-mode 0)))
+
+(add-hook 'term-mode-hook
+	  (lambda ()
+	    (font-lock-mode 0)) t)
+
 (add-hook 'after-save-hook
           'executable-make-buffer-file-executable-if-script-p)
 
