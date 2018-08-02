8528d90f2910276e56bdaf10a1c46c2c709be32a
Ioan-Adrian Ratiu <adi@adirat.com>
2018-08-02 16:43:41 +0300

    shell: remove modeline dirtracking
    Signed-off-by: Ioan-Adrian Ratiu <adi@adirat.com>

--- lisp/98-shell.el
+++
@@ -1,5 +0,0 @@
-(defun add-mode-line-dirtrack ()
-  (setq mode-line-buffer-identification
-        '(:propertize default-directory face mode-line-buffer-id)))
-
-(add-hook 'shell-mode-hook 'add-mode-line-dirtrack)
