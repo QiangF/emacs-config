29c9aa6ea1abd11bbdae26c90f088e16b25844c0
Ioan-Adrian Ratiu <adi@adirat.com>
2018-08-06 16:03:43 +0300

    dired: add jump shortcuts
    Signed-off-by: Ioan-Adrian Ratiu <adi@adirat.com>

--- lisp/98-dired.el
+++ lisp/98-dired.el
@@ -4,3 +4,7 @@
       dired-recursive-deletes 'always)
 
 (put 'dired-find-alternate-file 'disabled nil) ; allow 'a' cmd
+
+(define-key dired-mode-map (kbd "C-l") 'dired-jump)
+(global-set-key (kbd "C-x l") 'dired-jump)
+(global-set-key (kbd "C-x C-l") 'dired-jump)
