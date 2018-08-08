e9575c53418f8029c27e866c3d495e17ddde12cc
Ioan-Adrian Ratiu <adi@adirat.com>
2018-08-08 22:11:25 +0300

    dired: directly asign 'a' cmd function
    Signed-off-by: Ioan-Adrian Ratiu <adi@adirat.com>

--- lisp/98-dired.el
+++ lisp/98-dired.el
@@ -3,9 +3,9 @@
 (setq dired-listing-switches "-alh"
       dired-recursive-deletes 'always)
 
-(put 'dired-find-alternate-file 'disabled nil) ; allow 'a' cmd
-
 (define-key dired-mode-map (kbd "C-l") 'dired-jump)
 (global-set-key (kbd "C-x l") 'dired-jump)
 (global-set-key (kbd "C-x C-l") 'dired-jump)
+
+(define-key dired-mode-map "a" 'dired-find-file)
 (define-key dired-mode-map "f" 'find-file)
