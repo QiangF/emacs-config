753a4715094b7e3a7dca395f0fa4fc19b5a1cc71
Ioan-Adrian Ratiu <adi@adirat.com>
2018-08-31 16:14:01 +0300

    winum: remove
    Signed-off-by: Ioan-Adrian Ratiu <adi@adirat.com>

--- lisp/10-ui.el
+++ lisp/10-ui.el
@@ -75,40 +75,12 @@
 
 (global-eldoc-mode -1)
 
-(setq winum-keymap
-      (let ((map (make-sparse-keymap)))
-	(define-key map (kbd "C-`") 'winum-select-window-by-number)
-	(define-key map (kbd "C-²") 'winum-select-window-by-number)
-	(define-key map (kbd "M-0") 'winum-select-window-0-or-10)
-	(define-key map (kbd "M-1") 'winum-select-window-1)
-	(define-key map (kbd "M-2") 'winum-select-window-2)
-	(define-key map (kbd "M-3") 'winum-select-window-3)
-	(define-key map (kbd "M-4") 'winum-select-window-4)
-	(define-key map (kbd "M-5") 'winum-select-window-5)
-	(define-key map (kbd "M-6") 'winum-select-window-6)
-	(define-key map (kbd "M-7") 'winum-select-window-7)
-	(define-key map (kbd "M-8") 'winum-select-window-8)
-	(define-key map (kbd "M-9") 'winum-select-window-9)
-	map))
-
-(require 'winum)
-
-(setq window-numbering-scope            'frame-local
-      winum-auto-assign-0-to-minibuffer t
-      winum-auto-setup-mode-line        nil)
-
-(set-face-attribute 'winum-face nil :weight 'bold)
-
-(winum-mode)
-
 (minibuffer-line-mode)
 
 (global-set-key (kbd "S-C-<left>") 'shrink-window-horizontally)
 (global-set-key (kbd "S-C-<right>") 'enlarge-window-horizontally)
 (global-set-key (kbd "S-C-<up>") 'shrink-window)
 (global-set-key (kbd "S-C-<down>") 'enlarge-window)
-
-(global-unset-key "\C-x\o")
 
 (setq enable-recursive-minibuffers t)
 
