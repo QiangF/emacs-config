1a9d8b5b572315200181cc20937c71af7b4e1173
Ioan-Adrian Ratiu <adi@adirat.com>
2019-01-25 02:39:23 +0200

    sh: cleanup & enable font coloring
    Signed-off-by: Ioan-Adrian Ratiu <adi@adirat.com>

--- lisp/98-sh.el
+++ lisp/98-sh.el
@@ -16,25 +16,13 @@
 
 (add-to-list 'eshell-preoutput-filter-functions 'xterm-color-filter)
 
-(defun clear-shell-screen ()
-  (interactive)
-  (end-of-buffer)
-  (recenter-top-bottom 0))
-
 (add-hook 'shell-mode-hook
 	  (lambda ()
-	    (add-hook 'comint-preoutput-filter-functions 'xterm-color-filter nil t)
-	    (font-lock-mode 0)
-	    (define-key comint-mode-map "\C-l" 'clear-shell-screen)))
+	    (add-hook 'comint-preoutput-filter-functions 'xterm-color-filter nil t)))
 
 (add-hook 'eshell-before-prompt-hook
-	  (lambda ()
-	    (setq xterm-color-preserve-properties t)
-	    (font-lock-mode 0)))
-
-(add-hook 'term-mode-hook
-	  (lambda ()
-	    (font-lock-mode 0)) t)
+ 	  (lambda ()
+ 	    (setq xterm-color-preserve-properties t)))
 
 (add-hook 'after-save-hook
           'executable-make-buffer-file-executable-if-script-p)
