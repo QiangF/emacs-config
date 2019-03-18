8c5d3b5b25b1f2bd3a38bbaedda8578b3cb216c4
Adrian Ratiu <adrian.ratiu@collabora.com>
2019-03-18 14:57:49 +0200

    smerge: drop conflict background colors
    Signed-off-by: Adrian Ratiu <adrian.ratiu@collabora.com>

--- lisp/98-magit.el
+++ lisp/98-magit.el
@@ -1,5 +1,10 @@
 (require 'magit)
+(require 'smerge-mode)
 
 (global-set-key (kbd "C-x g") 'magit-status)
 
 (setq magit-commit-arguments '("--signoff"))
+
+(set-face-attribute 'smerge-lower nil :background nil)
+(set-face-attribute 'smerge-base nil :background nil)
+(set-face-attribute 'smerge-upper nil :background nil)
