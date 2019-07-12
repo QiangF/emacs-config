3fc563b5ef389f8521a0d98c09e97f3c22ca1f7a
Adrian Ratiu <adi@adirat.com>
2019-07-13 02:34:35 +0300

    lisp/00-paths: drop bin dir
    Signed-off-by: Adrian Ratiu <adi@adirat.com>

--- lisp/00-paths.el
+++ lisp/00-paths.el
@@ -2,8 +2,7 @@
       temporary-file-directory (concat user-emacs-directory "tmp/")
       config-file-directory (concat user-emacs-directory "config/")
       lib-file-directory (concat user-emacs-directory "lib")
-      lisp-file-directory (concat user-emacs-directory "lisp")
-      binary-file-directory (concat user-emacs-directory "bin"))
+      lisp-file-directory (concat user-emacs-directory "lisp"))
 
 (let ((lib-paths (directory-files lib-file-directory t directory-files-no-dot-files-regexp))
       (non-std-lib-paths (mapcar (apply-partially 'concat user-emacs-directory)
@@ -11,8 +10,3 @@
 				"lib/notmuch/emacs"
 				"lisp"))))
   (mapc (apply-partially 'add-to-list 'load-path) (append non-std-lib-paths lib-paths)))
-
-(add-to-list 'exec-path binary-file-directory)
-
-(unless (string-match-p (regexp-quote binary-file-directory) (getenv "PATH"))
-  (setenv "PATH" (concat (getenv "PATH") ":" binary-file-directory)))
