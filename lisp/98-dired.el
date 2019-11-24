a49b602851cfac95cdc5231bac80e4834ad2fd5b
Adrian Ratiu <adi@adirat.com>
2019-11-24 21:39:20 +0200

    dired: remove shplay logic
    Not used anymore
    
    Signed-off-by: Adrian Ratiu <adi@adirat.com>

--- lisp/98-dired.el
+++ lisp/98-dired.el
@@ -10,26 +10,7 @@
 (define-key dired-mode-map "a" 'dired-find-file)
 (define-key dired-mode-map "f" 'find-file)
 
-(setq shplay-hostname nil)
-
-(defun stop-play-file ()
   (interactive)
-  (when shplay-hostname
-    (start-process "shplay-remote" nil "shplay-remote" shplay-hostname "pkill" "playhrt")
-    (setq shplay-hostname nil)))
 
-(defun play-file ()
-  (interactive)
-  (when (dired-get-marked-files)
-    (let* ((diredfiles (dired-get-marked-files))
-	   (userhost (cadr (split-string (car diredfiles) ":")))
-	   (files (mapcar (lambda (f) (car (last (split-string f ":")))) diredfiles))
-	   (filesstr (string-join files "|||")))
-      (when shplay-hostname (stop-play-file) (sleep-for 2))
-      (start-process "shplay-remote" nil "shplay-remote" userhost "shplay" filesstr)
-      (setq shplay-hostname userhost))))
-
-(define-key dired-mode-map (kbd "P") 'play-file)
-(define-key dired-mode-map (kbd "S") 'stop-play-file)
 
 (define-key dired-mode-map (kbd "c") 'find-file)
