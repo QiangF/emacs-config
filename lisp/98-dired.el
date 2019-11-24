12896d344a03f92b59044bbd4af50124103a67a4
Adrian Ratiu <adi@adirat.com>
2019-11-24 21:48:20 +0200

    dired: group & cleanup key definitions
    Signed-off-by: Adrian Ratiu <adi@adirat.com>

--- lisp/98-dired.el
+++ lisp/98-dired.el
@@ -3,13 +3,6 @@
 (setq dired-listing-switches "-alh"
       dired-recursive-deletes 'always)
 
-(define-key dired-mode-map (kbd "C-l") 'dired-jump)
-(global-set-key (kbd "C-x l") 'dired-jump)
-(global-set-key (kbd "C-x C-l") 'dired-jump)
-
-(define-key dired-mode-map "a" 'dired-find-file)
-(define-key dired-mode-map "f" 'find-file)
-
 (defun my-dired-open-external ()
   "In Dired, visit the file or directory named on this line."
   (interactive)
@@ -27,9 +20,14 @@
       (setq prog_val (read-shell-command "Shell command: " nil nil)))
     (start-process prog_val nil prog_val (dired-get-filename))))
 
-
+(define-key dired-mode-map "a" 'dired-find-file)
+(define-key dired-mode-map "f" 'find-file)
 (define-key dired-mode-map "o" 'my-dired-open-external)
-(define-key dired-mode-map (kbd "c") 'find-file)
+(define-key dired-mode-map "c" 'find-file)
+
+(define-key dired-mode-map (kbd "C-l") 'dired-jump)
+(global-set-key (kbd "C-x l") 'dired-jump)
+(global-set-key (kbd "C-x C-l") 'dired-jump)
 
 (setq dired-guess-shell-alist-user '(("\\.pdf\\'" "evince")
                                      ("\\.doc?x\\'\\|\\.ppt?x\\'\\|\\.xls?x\\'" "libreoffice")
