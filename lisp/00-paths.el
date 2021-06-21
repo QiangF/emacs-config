1aafd31abec5b77c7bad9e72825a8eb3e7502eff
Adrian Ratiu <adi@adirat.com>
2021-06-21 21:27:55 +0300

    Revert "lisp/00-paths: drop bin dir"
    This reverts commit 3fc563b5ef389f8521a0d98c09e97f3c22ca1f7a.

--- lisp/00-paths.el
+++ lisp/00-paths.el
@@ -2,7 +2,8 @@
       temporary-file-directory (concat user-emacs-directory "tmp/")
       config-file-directory (concat user-emacs-directory "config/")
       lib-file-directory (concat user-emacs-directory "lib")
-      lisp-file-directory (concat user-emacs-directory "lisp"))
+      lisp-file-directory (concat user-emacs-directory "lisp")
+      binary-file-directory (concat user-emacs-directory "bin"))
 
 (let ((lib-paths (directory-files lib-file-directory t directory-files-no-dot-files-regexp))
       (non-std-lib-paths (mapcar (apply-partially 'concat user-emacs-directory)
@@ -11,3 +12,8 @@
 				   "lib/notmuch/emacs"
 				   "lisp"))))
   (mapc (apply-partially 'add-to-list 'load-path) (append non-std-lib-paths lib-paths)))
+
+(add-to-list 'exec-path binary-file-directory)
+
+(unless (string-match-p (regexp-quote binary-file-directory) (getenv "PATH"))
+  (setenv "PATH" (concat (getenv "PATH") ":" binary-file-directory)))
