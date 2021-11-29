c771e8410276f33fcc5093d44cfdf051c07b3db3
Adrian Ratiu <adi@adirat.com>
2021-11-30 00:17:25 +0200

    rust-mode: misc additions

--- lisp/98-rust.el
+++ lisp/98-rust.el
@@ -1,2 +1,10 @@
 (require 'rust-mode)
+
 (add-to-list 'auto-mode-alist '("\\.rs\\'" . rust-mode))
+
+(add-hook 'rust-mode-hook
+          (lambda () (setq indent-tabs-mode nil)))
+
+(setq rust-format-on-save t)
+
+;(define-key rust-mode-map (kbd "C-c C-r") 'rust-run)
